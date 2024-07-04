bbb367f8f1d18aeeba56056c80fd8c05ef120f0b
limematheuz <mat.rib.lima@gmail.com>
2024-07-04 13:40:15 +0200

    create update method

--- DroughtProject/Repositories/IUsersRepository.cs
+++ DroughtProject/Repositories/IUsersRepository.cs
@@ -5,4 +5,8 @@ namespace DroughtProject.Repositories;
 public interface IUsersRepository
 {
     Task<List<Users>> GetAllUsers();
+    Task<int> CreateUsers(Users users);
+    Task<int> UpdateUsers(Users users);
+    Task<bool> ExistUser(int id);
+
 }
